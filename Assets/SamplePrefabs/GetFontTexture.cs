a5020db08d10376364d5a77e3453c811bfcbe45b
Remy Maetz <remym@unity3d.com>
2019-12-06 10:47:00 +0100

    Fix texts in prefabs

---
+++ Assets/SamplePrefabs/GetFontTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(TextMesh))]
+public class GetFontTexture : MonoBehaviour
+{
+    public string targetTextureProperty = "_BaseMap";
+
+    private void OnValidate()
+    {
+        GetComponent<Renderer>().sharedMaterial.SetTexture(targetTextureProperty, GetComponent<TextMesh>().font.material.mainTexture);
+    }
+}
